4d665283a6d3dcb9c3da7c063c600c7110d723bd
Vasya <trata@yahoo.com>
2019-06-07 23:13:27 +0300

    spring start

---
+++ src/main/java/com/mycompany/AppConfig.java
@@ -0,0 +1,21 @@
+
+package com.mycompany;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.mycompany")
+public class AppConfig {
+    @Bean
+    public MessageRenderer messageRenderer() {
+        return new MessageRendererErrorImpl(message());
+    }
+
+    @Bean
+    public Message message() {
+        return new HelloMessage("Hello!");
+    }
+}
---
+++ src/main/java/com/mycompany/IndependentMessageRenderer.java
@@ -0,0 +1,16 @@
+
+package com.mycompany;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IndependentMessageRenderer {
+
+    @Autowired
+    private MessageRenderer renderer;
+
+    public void print() {
+        renderer.printMessage();
+    }
+}
